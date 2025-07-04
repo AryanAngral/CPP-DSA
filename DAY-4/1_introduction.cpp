48c7b56be2120de164ff78f1e8e7cc95786c466c
AryanAngral <pawankumarangral@gmail.com>
2025-07-04 13:57:01 +0530

    notes uploaded

---
+++ DAY-4/1_introduction.cpp
@@ -0,0 +1,19 @@
+//System Information
+//Memory information
+//Built-In Information
+//Important Declaration/Dec[n]
+
+#include<iostream>
+#include<stdio.h>
+//Symbolic Constant Dec[n]
+//Pre-Processor Declaration
+//Function Prototype Declaration
+
+//namespace dec[n]
+using namespace std;
+//main dec[n]
+int main(){
+    // printf("Hello, Bolo..!");
+    cout << "Hello, Bolo...!";
+    return 0;
+}
